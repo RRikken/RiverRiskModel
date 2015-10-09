43eb70a7d2bd5f74b554bdc9f6637489e8a274b1
Fold77 <dj_flisijn@hotmail.com>
2015-10-09 14:37:22 +0200

    DikeBreach Function Scenario Piping

---
+++ MATLAB/Classes/DikeBreach.m
@@ -0,0 +1,25 @@
+classdef DikeBreach
+    %DIKEBREACH Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+        function WaterHeightDifference = CalculateSellmeijer(obj, SeepageLength, ThicknessSandLayer, RollResistanceAngle, TowForceFactor, WeightGrainUnderwater, WeightWater, IntrinsicConductivity, PercentileSand)
+            Alpha = CalculateAlpha(ThicknessSandLayer, SeepageLength);
+            Beta = CalculateBeta(TowForceFactor, PercentileSand, IntrinsicConductivity, SeepageLength);
+            WaterHeightDifference = Alpha*Beta*(WeightGrainUnderwater/WeightWater)*tan(RollResistanceAngle)*(0.68-0.10*log(Beta))*SeepageLength;
+        end
+        
+        function Alpha = CalculateAlpha(obj, ThicknessSandLayer, SeepageLength)
+            Alpha = (ThicknessSandLayer/SeepageLength)^(0.28/((ThicknessSandlayer/SeepageLength)^2.8)-1);
+        end
+        
+        function Beta = CalculateBeta(obj, TowForceFactor, PercentileSand, IntrinsicConductivity, SeepageLength)
+            Beta = TowForceFactor*PercentileSand*(1/(IntrinsicConductivity*SeepageLength))^(1/3);
+        end
+    end
+    
+end
+
