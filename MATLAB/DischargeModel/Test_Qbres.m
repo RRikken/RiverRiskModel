bfa00b071b43de82d809f44acdd1e8e2c2855506
Fold77 <dj_flisijn@hotmail.com>
2015-10-30 12:05:20 +0100

    qbres en gestuwde-ongestuwde instroom bepalen

--- MATLAB/DischargeModel/Test_Qbres.m
+++ MATLAB/DischargeModel/Test_Qbres.m
@@ -4,11 +4,28 @@ clear,clc
 gravitation = 9.81;
 WidthBres = 300;
 DeltaH1 = 1.3;
-DeltaH2 = 3.05;
+DeltaH2 = 0.58;
 HeightBresIn1 = 11.61;
 HeightBresIn2 = 8.58;
-
+HeightBresOut1 = 6.06;
+HeightBresOut2 = 4.85;
 %Formules
 FlowBres1 = ((2*gravitation)^0.5)*WidthBres*DeltaH1*HeightBresIn1
 
 FlowBres2 = ((2*gravitation)^0.5)*WidthBres*DeltaH2*HeightBresIn2
+
+opgave = (2/3)^1.5 * 9.81^0.5 * 300 * 4.85^1.5;
+
+%ongestuwde- of gestuwde instroom
+
+if HeightBresIn1 < (2/3)*HeightBresOut1;
+    disp(1)
+else HeightBresIn1 > (2/3)*HeightBresOut1;
+    disp(2)
+end
+
+if HeightBresIn2 < (2/3)*HeightBresOut2;
+    disp(1)
+else HeightBresIn2 > (2/3)*HeightBresOut2;
+    disp(2)
+end
