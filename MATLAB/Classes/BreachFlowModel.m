a182065019076872238c2937c5f68d5c3842f801
Rob Rikken <webmaster@vvdrienerlo.nl>
2015-11-23 11:04:14 +0100

    Added breachmodel class version

---
+++ MATLAB/Classes/BreachFlowModel.m
@@ -0,0 +1,62 @@
+classdef BreachFlowModel < handle
+    %BREACHFLOWMODEL Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Access = private)
+        WidthBreach
+        HeightBreach
+        WaterLevelRiver
+        WaterLevelDikeRingArea
+    end
+    
+    properties (Access = private, Dependent)
+        WaterLevelDifference
+        WaterLevelInsideBreach
+        WaterLevelOusideBreach
+    end
+    
+    properties (Access = private, Constant)
+        Gravity = 9.81;
+    end
+    
+    methods
+        function obj = BreachFlowModel(WidthBreach, HeightBreach)
+            obj.WidthBreach = WidthBreach;
+            obj.HeightBreach = HeightBreach;
+            obj.WaterLevelRiver = 0;
+            obj.WaterLevelDikeRingArea = 0;
+        end
+        
+        function set.WaterLevelRiver(obj, WaterLevelRiver)
+            obj.WaterLevelRiver = WaterLevelRiver;
+        end
+         function set.WaterLevelDikeRingArea(obj, WaterLevelRiver)
+            obj.WaterLevelDikeRingArea = WaterLevelRiver;
+         end
+        
+        function ReturnedWaterLevelDifference =  get.WaterLevelDifference(obj)
+            ReturnedWaterLevelDifference = obj.WaterLevelRiver - obj.WaterLevelDikeRingArea;
+        end
+        function ReturnedWaterLevelInsideBreach = get.WaterLevelInsideBreach(obj)
+            ReturnedWaterLevelInsideBreach = obj.HeightBreach - obj.WaterLevelDikeRingArea;
+        end
+        function ReturnedWaterLevelOusideBreach = get.WaterLevelOusideBreach(obj)
+            ReturnedWaterLevelOusideBreach = obj.HeightBreach - obj.WaterLevelRiver;
+        end
+        
+        function FlowThroughBreach = CalculateFlowThroughBreach(obj)
+            % Debietformule (Visser, 1998; Ren, 2012)
+            % Qbres = 	(2/3)^1,5 ? g^0,5 ? Bbres ? hbr_uit^1,5	als hbr_in  < ? ? hbr_uit	ongestuwde instroom
+            % Qbres = 	(2?g)^0,5 ? Bbres ?  ?h^0,5? hbr_in als hbr_in  > ? ? hbr_uit	gestuwde instroom
+           
+            if obj.WaterLevelInsideBreach < (2/3) * obj.WaterLevelOusideBreach
+                FlowThroughBreach = (2/3)^(1.5) * obj.Gravity^(0.5) * obj.WidthBreach * obj.WaterLevelOusideBreach^(1.5);
+            elseif obj.WaterLevelInsideBreach >= (2/3) * obj.WaterLevelOusideBreach
+                FlowThroughBreach = ((2 * obj.Gravity)^0.5) * obj.WidthBreach * obj.WaterLevelDifference^(0.5) * obj.WaterLevelInsideBreach;
+            else
+                debug
+            end
+        end
+    end
+    
+end
